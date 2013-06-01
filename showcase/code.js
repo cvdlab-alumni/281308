1a7380410ede4d803eb22ad22844013b945408c3
Federico <federico.violante@qurami.net>
2013-06-01 18:07:24 +0200

    neck

--- showcase/code.js
+++ showcase/code.js
@@ -31,6 +31,7 @@ var doorsect = 36;
 
 //Colori
 var BLACK = [0,0,0,1];
+var WHITE = [2,2,2,1];
 
 //****MODELLO****//
 
@@ -39,7 +40,7 @@ var BLACK = [0,0,0,1];
 universe = function(){
 	var universeDom = DOMAIN([[0,PI],[0,2*PI]])([3,4]);
 	var universe = Sk(10000)(MAP(SPHERE)(universeDom));
-	return COLOR([0,0,0,1])(universe);
+	return COLOR(BLACK)(universe);
 };
 //DRAW(universe());
 
@@ -50,15 +51,29 @@ earth = function(){
 };
 //DRAW(earth());
 
+atmosphere = function(){
+	var atmosphereDom = DOMAIN([[0,PI/2.0],[0,2*PI]])([72,172]);
+	var atmosphere = T([2])([-12000])(Sk(11000)(MAP(SPHERE)(atmosphereDom)));
+	return COLOR255([200, 240, 255,0.65])(atmosphere);
+};
+//DRAW(atmosphere());
+
+
 //Bubbleship
 
 tetrahedralCore = function(){
-	var core_faceX = TRIANGLE_DOMAIN(1, [[1,0,0],[2,0,SQRT(2)],[0,0,SQRT(2)]]);
-	var core_faceYL = TRIANGLE_DOMAIN(1, [[1,0,0],[1,SQRT(2),SQRT(2)],[2,0,SQRT(2)]]);
-	var core_faceYR = TRIANGLE_DOMAIN(1, [[1,0,0],[1,SQRT(2),SQRT(2)],[0,0,SQRT(2)]]);
-	var core_faceZ = TRIANGLE_DOMAIN(1, [[2,0,SQRT(2)],[1,SQRT(2),SQRT(2)],[0,0,SQRT(2)]]);
-	var core = STRUCT([core_faceX, core_faceYL, core_faceYR, core_faceZ]);
-	return COLOR([1,1,1,1])(core);
+
+var p1 = [1,0,0];
+var p2 = [2.5,0,SQRT(2.5)];
+var p3 = [-0.5,0,SQRT(2.5)];
+var p4 = [1,SQRT(3),SQRT(2.5)];
+
+	var core_faceX =  TRIANGLE_DOMAIN(1, [p1, p2, p3]);
+	var core_faceYL = TRIANGLE_DOMAIN(1, [p1, p4, p2]);
+	var core_faceYR = TRIANGLE_DOMAIN(1, [p1, p4, p3]);
+	var core_faceZ =  TRIANGLE_DOMAIN(1, [p2 ,p4, p3]);
+	var core = T([1])([0.75])(STRUCT([core_faceX, core_faceYL, core_faceYR, core_faceZ]));
+	return COLOR(WHITE)(core);
 };
 DRAW(tetrahedralCore());
 
@@ -74,48 +89,54 @@ cockpit = function(){
 	var cockpitGlass = COLOR255([160,210,255,0.5])(STRUCT([cockpitBubble, cockpitDoor]));
 
 	//Frame
-	var cockpitDoorFrameDom = DOMAIN([[3*PI/4.0,(3*PI+0.4)/4.0],[0,2*PI]])([framesect,sect]);
-	var cockpitDoorFrame = T([0,1,2])([-0.3,0,-PI/4.5])(R([0,2])(-PI/4.0)(MAP(SPHERE)(cockpitDoorFrameDom)));
+	// var cockpitDoorFrameDom = DOMAIN([[3*PI/4.0,(3*PI+0.4)/4.0],[0,2*PI]])([framesect,sect]);
+	// var cockpitDoorFrame = T([0,1,2])([-0.3,0,-PI/4.5])(R([0,2])(-PI/4.0)(MAP(SPHERE)(cockpitDoorFrameDom)));
 
-	var cockpitClosedDoorFrameDom = DOMAIN([[(PI-0.4)/4.0, PI/4.0],[0,2*PI]])([framesect,sect]);
-	var cockpitClosedDoorFrame = MAP(SPHERE)(cockpitClosedDoorFrameDom);
+	// var cockpitClosedDoorFrameDom = DOMAIN([[(PI-0.4)/4.0, PI/4.0],[0,2*PI]])([framesect,sect]);
+	// var cockpitClosedDoorFrame = MAP(SPHERE)(cockpitClosedDoorFrameDom);
 
-	var cockpitFrames = COLOR([1,1,1,1])(STRUCT([cockpitDoorFrame, cockpitClosedDoorFrame]));
+	// var cockpitFrames = COLOR([1,1,1,1])(STRUCT([cockpitDoorFrame, cockpitClosedDoorFrame]));
 
-	var IncockpitDoorFrameDom = DOMAIN([[(3*PI-0.1)/4.0,(3*PI+0.5)/4.0],[0,2*PI]])([framesect,sect]);
-	var IncockpitDoorFrame = T([0,1,2])([-0.3,0,-PI/4.5])(R([0,2])(-PI/4.0)(MAP(SPHERE)(IncockpitDoorFrameDom)));
+	// var IncockpitDoorFrameDom = DOMAIN([[(3*PI-0.1)/4.0,(3*PI+0.5)/4.0],[0,2*PI]])([framesect,sect]);
+	// var IncockpitDoorFrame = T([0,1,2])([-0.3,0,-PI/4.5])(R([0,2])(-PI/4.0)(MAP(SPHERE)(IncockpitDoorFrameDom)));
 
-	var IncockpitClosedDoorFrameDom = DOMAIN([[(PI-0.5)/4.0, (PI+0.1)/4.0],[0,2*PI]])([framesect,sect]);
-	var IncockpitClosedDoorFrame = MAP(SPHERE)(IncockpitClosedDoorFrameDom);
+	// var IncockpitClosedDoorFrameDom = DOMAIN([[(PI-0.5)/4.0, (PI+0.1)/4.0],[0,2*PI]])([framesect,sect]);
+	// var IncockpitClosedDoorFrame = MAP(SPHERE)(IncockpitClosedDoorFrameDom);
 
-	var IncockpitBackDom = DOMAIN([[3*PI/4,PI],[0,2*PI]])([doorsect,sect]);
-	var IncockpitBack = R([0,2])(-PI/2.0)(MAP(SPHERE)(IncockpitBackDom));
+	// var IncockpitBackDom = DOMAIN([[3*PI/4.0,PI],[0,2*PI]])([doorsect,sect]);
+	// var IncockpitBack = R([0,2])(-PI/2.0)(MAP(SPHERE)(IncockpitBackDom));
 
-	var IncockpitFloorDom = DOMAIN([[3*PI/4,PI],[0,2*PI]])([doorsect,sect]);
-	var IncockpitFloor = R([1,2])(-PI/2.0)(MAP(SPHERE)(IncockpitFloorDom));
+	// var IncockpitFloorDom = DOMAIN([[3*PI/4.0,PI],[0,2*PI]])([doorsect,sect]);
+	// var IncockpitFloor = R([1,2])(-PI/2.0)(MAP(SPHERE)(IncockpitFloorDom));
 
-	var IncockpitUpDom = DOMAIN([[(PI-0.25)/2.0,(PI+0.25)/2.0],[PI/4,3*PI/4]])([framesect,sect]);
-	var IncockpitUp = R([0,2])(-PI/2.0)(MAP(SPHERE)(IncockpitUpDom));
+	// var IncockpitUpDom = DOMAIN([[(PI-0.25)/2.0,(PI+0.25)/2.0],[PI/4,3*PI/4.0]])([framesect,sect]);
+	// var IncockpitUp = R([0,2])(-PI/2.0)(MAP(SPHERE)(IncockpitUpDom));
 
-	var IncockpitUpBackDom = DOMAIN([[(PI-0.5)/2.0,(PI+0.5)/2.0],[0,PI]])([framesect,sect]);
-	var IncockpitUpBack = R([0,1])(-PI/2.0)(MAP(SPHERE)(IncockpitUpBackDom));
+	// var IncockpitUpBackDom = DOMAIN([[(PI-0.5)/2.0,(PI+0.5)/2.0],[0,PI]])([framesect,sect]);
+	// var IncockpitUpBack = R([0,1])(-PI/2.0)(MAP(SPHERE)(IncockpitUpBackDom));
 
-	var IncockpitCircleUpDom = DOMAIN([[(PI-0.4),PI],[0,2*PI]])([framesect,sect]);
-	var IncockpitCircleUp = R([1,2])(PI/2.0)(MAP(SPHERE)(IncockpitCircleUpDom));
+	// var IncockpitCircleUpDom = DOMAIN([[(PI-0.4),PI],[0,2*PI]])([framesect,sect]);
+	// var IncockpitCircleUp = R([1,2])(PI/2.0)(MAP(SPHERE)(IncockpitCircleUpDom));
 
-	var IncockpitTop = STRUCT([IncockpitUp, IncockpitUpBack, IncockpitCircleUp]);
+	// var IncockpitTop = STRUCT([IncockpitUp, IncockpitUpBack, IncockpitCircleUp]);
 
-	var IncockpitFrames = COLOR(BLACK)(Sk(0.999)(STRUCT([IncockpitDoorFrame, IncockpitClosedDoorFrame, IncockpitBack, IncockpitFloor, IncockpitTop])));
+	// var IncockpitFrames = COLOR(BLACK)(Sk(0.999)(STRUCT([IncockpitDoorFrame, IncockpitClosedDoorFrame, IncockpitBack, IncockpitFloor, IncockpitTop])));
 
-	var allCockpitFrames = STRUCT([cockpitFrames, IncockpitFrames]);
+	// var allCockpitFrames = STRUCT([cockpitFrames, IncockpitFrames]);
 
-	var cockpitObj = T([0,1,2])([1,-1,1])(R([0,2])(PI/2.0)(R([0,1])(PI/2.0)(STRUCT([cockpitGlass, allCockpitFrames]))));
+//	var cockpitObj = T([0,1,2])([1,-1,1])(R([0,2])(PI/2.0)(R([0,1])(PI/2.0)(STRUCT([cockpitGlass, allCockpitFrames]))));
+	var cockpitObj = T([0,1,2])([1,-1,1])(R([0,2])(PI/2.0)(R([0,1])(PI/2.0)(STRUCT([cockpitGlass]))));
 
 	return cockpitObj;
 };
-
 DRAW(cockpit());
 
-var points = [[0,0], [1,1], [2,0]];
-var polypoint = POLYPOINT(points);
-DRAW(polypoint);
+//Neck
+neck = function(){
+	var neckDom = DOMAIN([[0,1],[0,2*PI]])([2*sect,sect]);
+	var neckProfile = BEZIER(S0)([[3,3,0],[3,3,0.2],[3,3,0.2],[3,3,0.2], [1.5,1.5,0.8],[1.5,1.5,0.8], [2,2,2.5],[2,2,2.5], [2,2,3],[2,2,3],[2,2,3],[2,2,3], [3,3,4],[3,3,4],[3,3,4], [3,3,4.5],[3,3,4.5],[3,3,4.5]]);
+	var neckMap = ROTATIONAL_SURFACE(neckProfile);
+	var neckObj = MAP(neckMap)(neckDom);
+	return Sk(1/4.5)(T([0,1,2])([4.5,-1.15,4.5])(R([1,2])(-PI/2.0)(neckObj)));
+};
+DRAW(neck());
